3bd709e0010296dff39548a469fa170cab762d60
gorou0501 <gorou0501it@gmail.com>
2019-11-24 13:46:37 +0900

    1124

--- shop/shop_cartlook2.php
+++ shop/shop_cartlook2.php
@@ -13,7 +13,7 @@ else
     print 'ようこそ';
     print $_SESSION['member_name'];
     print '様';
-    print '<a href="member_login.php">ログアウト</a>'<br />;
+    print '<a href="member_login.php">ログアウト</a><br />';
     print '<br />';
 }
 ?>
@@ -25,23 +25,30 @@ else
 <body>
 <?php
 //try{
-
-$cart=$_SESSION['cart'];
-$max=count($cart);    
-// var_dump($cart);
-// exit();
-
+if(isset($_SESSION['cart'])==true)
+{  
+  $cart=$_SESSION['cart'];
+  $kazu=$_SESSION['kazu'];
+  $max=count($cart);   
+}
+else
+{
+  $max=0;
+}
+if($max==0){
+  print 'カートに商品が入っていません <br />';
+  print '<br />';
+  print '<a href="shop_list.php">商品一覧へ戻る</a>';
+  exit();
+}
 include '../database.php';
-
 foreach($cart as $key =>$val)
 {
   $sql='SELECT code,name,price,gazou FROM mst_product WHERE code=?';
   $stmt=$dbh->prepare($sql);
   $data[0]=$val;
   $stmt->execute($data);
-
   $rec=$stmt->fetch(PDO::FETCH_ASSOC);
-
   $pro_name[]=$rec['name'];
   $pro_price[]=$rec['price'];
   if($rec['gazou']==''){
@@ -59,24 +66,47 @@ catch(Exception $e)
     print 'ただいま障害によりご迷惑をお掛けしております。';
     exit();
 }*/
-
 //print '<input type="submit" value="修正">';
 //print '</form>';
 ?>
 カートの中身<br />
 <br />
+<form method="post" action="kazu_change.php">
+<table border="1">
+<tr>
+  <td>商品</td>
+  <td>商品画像</td>
+  <td>価格</td>
+  <td>数量</td>
+  <td>小計</td>
+  <td>削除</td>
+</tr>
+
 <?php for($i=0;$i<$max;$i++){
 ?>
- <?php print '$pro_name[$i]'; ?>
- <?php print '$pro_gazou[$i]'; ?>
- <?php print '$pro_price[$i].円';?>
- <?php print '<br />'; ?>
+
+<tr>
+ <td><?php print $pro_name[$i]; ?></td>
+ <td><?php print $pro_gazou[$i]; ?></td>
+ <td><?php print $pro_price[$i];?>円</td>
+ <td><input type="checkbox" name="sakujo<?php print $i;?>"></td>
  <br />
+ <td><input type="text" name="kazu<?php print $i;?>" value="<?php print $kazu[$i];?>"></td>
+ <td><?php print $pro_price[$i]*$kazu[$i]; ?></td>
+ <br />
+ <td><?php print $kazu[$i]; ?></td>
+ <td><?php print '<br />'; ?></td>
+ 
+</tr>
 <?php 
 }
 ?>
-<form>
+</table>
+<input type="hidden" name="max" value="<?php print $max; ?>">
+<input type="submit" value="数量変更"><br />
 <input type="button" onclick="history.back()" value="戻る">
 </form>
+<br />
+<a href="shop_from.html">ご購入手続きへ進む</a><br />
 </body>
 </html>
